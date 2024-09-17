9d5f3d0207572a91ae4b7a2bb5eb247fdf7ffee3
NyXull <drlivesey60@gmail.com>
2024-09-16 21:33:41 -0300

    Alteração de texto

--- App.tsx
+++ App.tsx
@@ -4,11 +4,15 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
+
+
+
+
 
 const styles = StyleSheet.create({
   container: {
