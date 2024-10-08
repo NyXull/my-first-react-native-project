e630f6ccc0eb31ec91cbfdc3c73307e03aa522ce
NyXull <drlivesey60@gmail.com>
2024-10-07 21:51:52 -0300

    Trabalhando com Hook useState

--- App.tsx
+++ App.tsx
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-
+import React from 'react';
 import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
+  let [newTask, setNewTask] = React.useState('');
+
   return (
     <View style={styles.container}>
-      <Text style={styles.textos}>Digite seu nome</Text>
+      {/*<Text style={styles.textos}>Digite seu nome</Text>
       <TextInput placeholder='Nome' style={styles.textinp}/>
       <Text style={styles.textos}>Digite sua data de nascimento</Text>
       <TextInput placeholder='DD-MM-AAAA' style={styles.textinp} />
@@ -14,9 +16,15 @@ export default function App() {
       <Text style={styles.textos}>Digite seu Email</Text>
       <TextInput placeholder='email@email.com' style={styles.textinp} />
       <Text style={styles.textos}>Digite seu telefone</Text>
-      <TextInput placeholder='+55 XXX XXXXX-XXXX' style={styles.textinp} />
-      <TouchableOpacity activeOpacity={0.7} style={styles.button}><Text style={styles.textos}>Enviar</Text></TouchableOpacity>
-
+      <TextInput placeholder='+55 XXX XXXXX-XXXX' style={styles.textinp} />*/}
+      <Text style={styles.textos}>Trabalhando com Hook useState</Text>
+      <TextInput onChangeText={setNewTask} placeholder='Tarefa' style={styles.textinp} />
+      <Text></Text>
+      <Text style={styles.textos}>Elemento digitado</Text>
+      <Text style={styles.textos}>{newTask}</Text>
+      {/*<TouchableOpacity activeOpacity={0.7} style={styles.button}>
+        <Text style={styles.textos}>Adicionar tarefa</Text>
+      </TouchableOpacity>*/}
       <StatusBar style="auto" />
     </View>
   );
@@ -25,7 +33,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#1C1C1',
+    backgroundColor: '#4F4F4F',
     alignItems: 'center',
     justifyContent: 'center',
   },
